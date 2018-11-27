91d20f6ed30d2ff9e7af9888444c90d12c444437
V.Ruzanov <v.ruzanov95@gmail.com>
2018-11-27 19:38:36 +0300

    refactor: py -> sh

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 # Graylog-Script
-Python with shell script to install Graylog 2.3 with Elasticsearch 5.X. Currently only tested on Ubuntu 17.10 64-bit but feedback on script would be awesome as I am learning more advanced python techniques.
+Python with shell script to install Graylog 2.4 with Elasticsearch 2.3.1 Currently only tested on Ubuntu 17.10 64-bit but feedback on script would be awesome as I am learning more advanced python techniques.
 
 
 **With Elasticsearch installed locally, server must have at least 4G of RAM
 
+
+First, get the script and make it executable :
 ```
-git clone https://github.com/rayl7786/Graylog-Script.git
-cd Graylog-Script
-python3 graylog.py
+curl -O https://raw.githubusercontent.com/Angristan/openvpn-install/master/openvpn-install.sh
+chmod +x graylog-install.sh
+chmod +x graylog-conf.sh
 ```
+
+Then run it :
+
+```./graylog-install.sh```
---
+++ graylog-install.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+# Update repository
+echo ""
+echo "*** Running apt update command ***"
+echo ""
+
+apt update
+
+# Upgrade packages
+echo ""
+echo "*** Running apt upgrade command ***"
+echo ""
+apt upgrade -y
+
+# Install necessary packages for complete graylog setup
+echo ""
+echo "*** Installing necessary packages ***"
+echo ""
+apt -y install apt-transport-https openjdk-8-jre-headless uuid-runtime pwgen
+
+# Install Elasticsearch locally
+echo ""
+echo "Would you like Elasticsearch installed locally?"
+echo "   1) Yes"
+echo "   2) No"
+
+until [[ "$INSTALL_ELASTICSEARCH" =~ ^[1-2]$ ]]; do
+  read -rp "Select an option [1-2]: " -e -i 1 INSTALL_ELASTICSEARCH
+done
+
+if [[ $INSTALL_ELASTICSEARCH = "1" ]]; then
+  echo ""
+  echo "*** Installing Elasticsearch ***"
+  echo ""
+
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.deb
+  wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.5.1.deb.sha512
+  shasum -a 512 -c elasticsearch-6.5.1.deb.sha512
+  dpkg -i elasticsearch-6.5.1.deb
+
+  sed -i 's|cluster.name: elasticsearch$|cluster.name: graylog|' /etc/elasticsearch/elasticsearch.yml
+
+  systemctl daemon-reload
+  systemctl enable elasticsearch.service
+  systemctl restart elasticsearch.service
+fi
+
+# Install MongoDB locally
+echo ""
+echo "Would you like MongoDB installed locally?"
+echo "   1) Yes"
+echo "   2) No"
+
+until [[ "$INSTALL_MONGODB" =~ ^[1-2]$ ]]; do
+  read -rp "Select an option [1-2]: " -e -i 1 INSTALL_MONGODB
+done
+
+if [[ $INSTALL_ELASTICSEARCH = "1" ]]; then
+  echo ""
+  echo "*** Installing MongoDB ***"
+  echo ""
+
+  apt -y install mongodb-server
+fi
+
+# Download graylog package
+
+echo ""
+echo "*** Downloading and Installing Graylog package ***"
+echo ""
+wget https://packages.graylog2.org/repo/packages/graylog-2.4-repository_latest.deb
+# Install graylog package
+dpkg -i graylog-2.4-repository_latest.deb
+apt -y update && apt install graylog-server
+
+# Update repository and install graylog
+apt update
+apt -y install graylog-server
+systemctl daemon-reload
+systemctl enable graylog-server.service
+
+# Configure graylog configuration file
+echo ""
+echo "*** Configuring Graylog ***"
+echo ""
+
+# Run shell script to configure server.conf
+sh graylog-conf.sh
+
+# Remove unnecessary files created during script
+rm "graylog-2.4-repository_latest.deb"
+rm "elasticsearch-6.5.1.deb"
+rm "elasticsearch-6.5.1.deb.sha512"
+
+# Start Elasticsearch & Graylog Server
+echo ""
+echo "*** Starting Elasticsearch ***"
+echo ""
+systemctl start elasticsearch.service
+
+echo ""
+echo "*** Starting Graylog ***"
+echo ""
+systemctl start graylog-server.service
